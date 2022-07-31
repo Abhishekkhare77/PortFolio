58a7d5d3fb12837237506b0f293dce6c76f34827
Abhishek khare <ghostabhi323@gmail.com>
2022-07-31 11:20:02 +0530

    Completed basic things

--- src/App.js
+++ src/App.js
@@ -16,6 +16,23 @@ function App() {
     setcursorX(e.pageX)
     setcursorY(e.pageY)
   })
+  const [mode,setmode] = useState('dark');
+  const [toggleSm,settoggleSm] = useState('sun');
+  const toggleMode = ()=>{
+    if(mode==='dark' && toggleSm==='sun'){
+      setmode('light');
+      document.body.style.backgroundColor='white';
+      document.body.style.color='black';
+      settoggleSm('moon');
+    }
+    else{
+      setmode('dark');
+      document.body.style.backgroundColor='black';
+      document.body.style.color='white';
+      settoggleSm('sun');
+    }
+  }
+
   return (
     <>
     <Router>
@@ -23,7 +40,7 @@ function App() {
           left:cursorX + 'px',
           top:cursorY + 'px'
         }}></div> 
-        <Navbar/>
+        <Navbar mode={mode} toggleMode={toggleMode} toggleSm={toggleSm}/>
             <Routes>
               <Route exact path="/" element={<Home/>}/>
               <Route exact path="/about" element={<About/>} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = (props) => {
+  
   return (
-    <nav>
+    <nav style={{color:props.mode==='light'?'black':'white'}}>
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/skills">Skills</a></li>
-        <i className="fa-solid fa-sun modeChanger"></i>
+        <i className={`fa-solid fa-${props.toggleSm} modeChanger`} onClick={props.toggleMode}></i>
+        <div className="mobile">
+          <a href="/"><i class="fa-solid fa-house"></i></a>
+          <a href="/about"><i class="fa-solid fa-address-card"></i></a>
+          <a href="/skills"><i class="fa-solid fa-lightbulb"></i></a>
+        </div>
       </ul>
     </nav>
   )
